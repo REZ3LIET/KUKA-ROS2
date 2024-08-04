37d61af90962f7499439343792e34c9a70c1c3b9
REZ3LIET <rz.samar.kale@gmail.com>
2024-08-04 10:22:36 +0000

    Integrated moveit

--- kuka_gazebo/launch/bringup.launch.py
+++ kuka_gazebo/launch/bringup.launch.py
@@ -5,7 +5,7 @@ import xacro
 from launch import LaunchDescription
 from launch.actions import IncludeLaunchDescription, RegisterEventHandler, ExecuteProcess
 from launch.launch_description_sources import PythonLaunchDescriptionSource
-from launch.substitutions import Command,LaunchConfiguration, PathJoinSubstitution, FindExecutable
+from launch.substitutions import LaunchConfiguration
 from launch_ros.actions import Node
 from launch.event_handlers import OnProcessExit
 from ament_index_python.packages import get_package_share_directory
