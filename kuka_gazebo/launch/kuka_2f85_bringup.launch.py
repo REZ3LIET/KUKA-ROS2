c105f08955d92f9ac8e9b91155ed254ac9f9b0f2
REZ3LIET <rz.samar.kale@gmail.com>
2024-12-18 17:05:47 +0000

    Added namespace for kuka arm with robotiq 2f 85

--- kuka_gazebo/launch/kuka_2f85_bringup.launch.py
+++ kuka_gazebo/launch/kuka_2f85_bringup.launch.py
@@ -1,35 +1,75 @@
 # Author: REZ3LIET
 
 import os
-import xacro
+import yaml
+import time
 from launch import LaunchDescription
 from launch.actions import IncludeLaunchDescription, RegisterEventHandler, ExecuteProcess, DeclareLaunchArgument
 from launch.launch_description_sources import PythonLaunchDescriptionSource
-from launch.substitutions import LaunchConfiguration, Command
+from launch.substitutions import LaunchConfiguration, Command, PathJoinSubstitution
 from launch_ros.actions import Node
 from launch.event_handlers import OnProcessExit
 from ament_index_python.packages import get_package_share_directory
- 
+from launch.conditions import IfCondition
+from launch.launch_context import LaunchContext
+
+def rewrite_yaml(source_file: str, root_key: LaunchConfiguration):
+    context = LaunchContext()
+    root_key = root_key.perform(context)
+    print(f"ROOT: {root_key}")
+    if not root_key:
+        return source_file
+
+    with open(source_file, 'r') as file:
+        ori_data = yaml.safe_load(file)
+    
+    updated_yaml = {root_key: ori_data}
+    dst_path = f"/tmp/{time.time()}.yaml"
+    with open(dst_path, 'w') as file:
+        yaml.dump(updated_yaml, file)
+    return dst_path
+
 def generate_launch_description():
+    ign_gz = LaunchConfiguration('ign_gz', default='True')
+    robot_name = LaunchConfiguration('robot_name', default='kuka_2f85_arm')
+    namespace = LaunchConfiguration('namespace', default='')
+    position_x = LaunchConfiguration('position_x', default='0.0')
+    position_y = LaunchConfiguration('position_y', default='0.0')
+    orientation_yaw = LaunchConfiguration('orientation_yaw', default='0.0')
+
     # Loading Gazebo
     ign_gazebo_node = IncludeLaunchDescription(
         PythonLaunchDescriptionSource(
             [os.path.join(get_package_share_directory("ros_gz_sim"), "launch"), "/gz_sim.launch.py"]
         ),
-        launch_arguments={'gz_args': '-r -v1'}.items() 
+        launch_arguments={'gz_args': '-r -v1'}.items(),
+        condition=IfCondition(ign_gz)
+    )
+
+    # Updating controller parameters
+    pkg_dir = get_package_share_directory('kuka_gazebo')
+    controller_file = rewrite_yaml(
+        source_file=os.path.join(pkg_dir, "config/kuka_2f85_controllers.yaml"),
+        root_key=namespace
     )
 
     # Loading Robot Model
-    pkg_dir = get_package_share_directory('kuka_gazebo')
     robot_xacro = Command([
-        'xacro ', os.path.join(pkg_dir, 'urdf/kr70_r2100_2f85.urdf.xacro')])
+        'xacro ', os.path.join(pkg_dir, 'urdf/kr70_r2100_2f85.urdf.xacro'),
+        ' robot_name:=', robot_name,
+        ' namespace:=', namespace,
+        ' controller_file:=', controller_file,
+        ])
     robot_description = {"robot_description": robot_xacro}
 
     # Publish TF
+    remappings = [("/tf", "tf"), ("/tf_static", "tf_static")]
     robot_state_publisher = Node(
         package="robot_state_publisher",
         executable="robot_state_publisher",
         name="robot_state_publisher",
+        namespace=namespace,
+        remappings=remappings,
         output="both",
         parameters=[robot_description]
     )
@@ -39,28 +79,29 @@ def generate_launch_description():
         package="ros_gz_sim",
         executable="create",
         arguments=[
-            "-topic", "robot_description",
-            "-name", "kuka_arm",
-            "-x", "0.0",
-            "-y", "0.0",
-            "-z", "0.0",
+            "-topic", PathJoinSubstitution([namespace, "/robot_description"]),
+            "-name", robot_name,
+            "-robot_namespace", namespace,
+            "-x", position_x,
+            "-y", position_y,
+            "-Y", orientation_yaw,
         ],
         output="both"
     )
 
     # Load Controllers
     joint_state_controller = ExecuteProcess(
-        cmd=['ros2', 'control', 'load_controller', '--set-state', 'active', 'joint_state_broadcaster'],
+        cmd=['ros2', 'control', 'load_controller', '--set-state', 'active', 'joint_state_broadcaster', '-c', PathJoinSubstitution([namespace, "/controller_manager"])],
         output="screen"
     )
 
     kuka_controller = ExecuteProcess(
-        cmd=['ros2', 'control', 'load_controller', '--set-state', 'active', 'kuka_arm_controller'],
+        cmd=['ros2', 'control', 'load_controller', '--set-state', 'active', 'kuka_arm_controller', '-c', PathJoinSubstitution([namespace, "/controller_manager"])],
         output="screen"
     )
 
     robotiq_controller = ExecuteProcess(
-        cmd=['ros2', 'control', 'load_controller', '--set-state', 'active', 'robotiq_2f85_controller'],
+        cmd=['ros2', 'control', 'load_controller', '--set-state', 'active', 'robotiq_2f85_controller', '-c', PathJoinSubstitution([namespace, "/controller_manager"])],
         output="screen"
     )
 
@@ -86,6 +127,13 @@ def generate_launch_description():
     )
 
     return LaunchDescription([
+        DeclareLaunchArgument('ign_gz', default_value=ign_gz),
+        DeclareLaunchArgument('robot_name', default_value=robot_name),
+        DeclareLaunchArgument('namespace', default_value=namespace),
+        DeclareLaunchArgument('position_x', default_value=position_x),
+        DeclareLaunchArgument('position_y', default_value=position_y),
+        DeclareLaunchArgument('orientation_yaw', default_value=orientation_yaw),
+
         ign_gazebo_node,
         robot_state_publisher,
         spawn_robot_node,
